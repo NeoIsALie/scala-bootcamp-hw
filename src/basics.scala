2a3f2ce4ca79245ca8abf950718ea167787ec519
Anastasiia Pokasova <pokasovaai@gmail.com>
2021-03-14 12:50:46 +0300

    Added hw1 task

---
+++ src/basics.scala
@@ -0,0 +1,5 @@
+  // Homework. Implement functions that calculate https://en.wikipedia.org/wiki/Least_common_multiple and
+  // https://en.wikipedia.org/wiki/Greatest_common_divisor for integers.
+
+  def lcm(a: Int, b: Int): Int = ???
+  def gcd(a: Int, b: Int): Int = ???
